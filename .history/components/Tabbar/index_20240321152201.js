efc1d6010c7b53daf61e7147686248e2d09913ca
Michelle2317 <michelle388q@gmail.com>
2024-03-21 15:32:21 -0700

    create basic setup

---
+++ .history/components/Tabbar/index_20240321152201.js
@@ -0,0 +1,7 @@
+export default function Tabbar() {
+  return (
+    <div>
+      <h1>Tabbar</h1>
+    </div>
+  );
+}
---
+++ components/Tabbar/index.js
@@ -0,0 +1,9 @@
+import styles from "./Tabbar.module.css";
+
+export default function Tabbar() {
+  return (
+        <>
+
+        </>
+  );
+}
