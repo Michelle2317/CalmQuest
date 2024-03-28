1af83321a383f4795e449c46fc4a5434249794c6
juliadeng123 <144835897+juliadeng123@users.noreply.github.com>
2024-03-28 13:30:12 -0700

    add content to homepage index.js

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,58 @@ export default function Home() {
       <main className={`${styles.main}`}>
         <div className={styles.mainContainer}>
           <Navbar />
-
-
+          <div className={styles.mainContent}>
+            <Image
+              src={`/images/placeholder.svg`}
+              alt="placeholder image"
+              width="431"
+              height="280"
+              className={styles.placeholder}
+            />
+          </div>
+          <div className={styles.helloContainer}>
+            <div className={styles.helloContainerLeft}>
+              <h1 className={styles.mainHello}>Hello Lorem</h1>
+              <p className={styles.mainQuote}>"A really inspiring quote" -random</p>
+            </div>
+            <Image
+              src={`/images/checkinPrimary.svg`}
+              alt="check in image"
+              width="25"
+              height="25"
+              className={styles.checkIn}
+            />
+            <h2 className={styles.checkIn}>Check-in</h2>
+          </div>
+          <div className={styles.journalingContainer}>
+            <p className={styles.prompt}>Journaling Prompt</p>
+            <p className={styles.quote}>Lorem ipsum dolor sit amet<br/> consectetur. Et ultrices sed tellus erat.</p>
+            <p className={styles.write}>Write &gt;</p>
+          </div>
+          <h4 className={styles.explore}>Let's Explore</h4>
+          <div className={styles.exerciseCarousel}>
+            <Image
+              src={`/images/placeholder.svg`}
+              alt="placeholder image"
+              width="191"
+              height="152"
+              className={styles.placeholder}
+            />
+            <Image
+              src={`/images/placeholder.svg`}
+              alt="placeholder image"
+              width="191"
+              height="152"
+              className={styles.placeholder}
+            />
+            <Image
+              src={`/images/placeholder.svg`}
+              alt="placeholder image"
+              width="191"
+              height="152"
+              className={styles.placeholder}
+            />
+          </div>
           <Tabbar />
         </div>
         
