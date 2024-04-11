85967d9184dd429204e10316b58bbbb3f88500d2
Michelle2317 <michelle388q@gmail.com>
2024-04-11 12:39:46 -0700

    remove extra folder

--- .history/pages/_app_20240411111737.js
+++
@@ -1,7 +0,0 @@
-import "@/styles/globals.css";
-import Navbar from '@/components/Navbar'
-import Tabbar from '@/components/Tabbar'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
